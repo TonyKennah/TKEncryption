9c47919247966387517d5c66a82cf4bd25d256a1
TonyKennah <tonykennah@hotmail.com>
2025-08-03 01:53:36 +0100

    not sure yet

--- src/main/java/uk/co/kennah/encrypt/Main.java
+++ src/main/java/uk/co/kennah/encrypt/Main.java
@@ -3,23 +3,16 @@ package uk.co.kennah.encrypt;
 public class Main {
 
 	public static void main(String[] args) {
-		// The master password should be retrieved from a secure location,
-		// such as an environment variable or a secrets management service.
-		// For this example, we'll hardcode it.
-		String masterPassword = "a-very-secret-master-password";
-
-		// The properties file will be created in the application's working directory.
-		SecureProperties sp = new SecureProperties("secure.properties", masterPassword);
-
-		// Set some properties
+		SecureProperties sp = SecureProperties.getInstance("file.txt");
         sp.setProperty("not.encrypted", "Readable value");
         sp.setEncryptedProperty("is.encrypted", "This will be unreadable in the file");
 
         // Save to the file
         sp.store();
-        System.out.println("Properties saved to secure.properties");
 
         // Reading properties
         System.out.println(sp.getProperty("is.encrypted")); // Prints the decrypted value
+
 	}
+
 }
